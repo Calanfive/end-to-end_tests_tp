0bbb032c17a6964163250238647c35082df59220
calanfive <camille.lelan@gmail.com>
2023-11-29 18:19:52 +0100

    test x2

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,18 +1,31 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('has title', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
+// });
+
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://playwright.dev/');
+
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
+
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  await expect(page.locator("#cpt")).toHaveText("0");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test2', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('button', { name: 'count is' }).click();
+  await page.getByRole('button', { name: 'count is' }).click();
+  await expect(page.locator("#cpt")).toHaveText("2");
 });
